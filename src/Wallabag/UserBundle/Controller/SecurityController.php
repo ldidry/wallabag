10223a84a88cd0a3c91e8a21dd3dc4cc43ff07c1
Luc Didry <luc@didry.org>
2020-10-25 11:09:17 +0100

    ✨ — Automatically redirect authenticated user from login to homepage

--- src/Wallabag/UserBundle/Controller/SecurityController.php
+++ src/Wallabag/UserBundle/Controller/SecurityController.php
@@ -11,11 +11,19 @@ class SecurityController extends FOSSecurityController
 {
     protected function renderLogin(array $data)
     {
-        return $this->render('FOSUserBundle:Security:login.html.twig',
-            array_merge(
-                $data,
-                ['registration_enabled' => $this->container->getParameter('wallabag_user.registration_enabled')]
-            )
-        );
+        /*
+         * IS_AUTHENTICATED_REMEMBERED: "All logged in users have this"
+         * https://symfony.com/doc/current/security.html#checking-to-see-if-a-user-is-logged-in-is-authenticated-fully
+         */
+        if (false === $this->container->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            return $this->render('FOSUserBundle:Security:login.html.twig',
+                array_merge(
+                    $data,
+                    ['registration_enabled' => $this->container->getParameter('wallabag_user.registration_enabled')]
+                )
+            );
+        } else {
+            return $this->redirect($this->generateUrl('homepage'));
+        }
     }
 }
